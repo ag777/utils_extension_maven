5ed63958f5f6b36186a8c850e73e2140bba3aea7
ag777 <ag777@vip.qq.com>
2023-03-23 17:10:21 +0800

    add ExcelUtils补充导出含有多个sheet的excel文件的重载方法

--- src/main/java/github/ag777/util/file/excel/ExcelUtils.java
+++ src/main/java/github/ag777/util/file/excel/ExcelUtils.java
@@ -36,6 +36,7 @@ public class ExcelUtils {
 //     * @param templatePath 模板文件路径
 //     * @param outputFile 输出文件路径
 //     * @param dataList 数据，每项对应sheet中的值
+//     * @return 根据模板生成的文件
 //     * @throws IOException io异常
 //     */
 //    public static File exportWithMultiSheet(String templatePath, File outputFile, List<Map<String, Object>> dataList) throws IOException {
@@ -52,7 +53,57 @@ public class ExcelUtils {
 //                realMap.put(i, map);
 //            }
 //        }
-//        Workbook workbook = ExcelExportUtil.exportExcel(realMap, params);
+//        return exportExcel(realMap, params, outputFile);
+//    }
+//
+//    /**
+//     * 导出含有多个sheet的excel文件(所有页共用一个数据,自动读取模板获取sheet数量)
+//     * @param templatePath 模板文件路径
+//     * @param outputFile 输出文件路径
+//     * @param dataMap 数据map
+//     * @return 根据模板生成的文件
+//     * @throws IOException io异常
+//     */
+//    public static File exportWithMultiSheet(String templatePath, File outputFile, Map<String, Object> dataMap) throws IOException {
+//        int sheetCount = getSheetCount(new File(templatePath));
+//        return exportWithMultiSheet(templatePath, outputFile, dataMap, sheetCount);
+//    }
+//
+//    /**
+//     * 导出含有多个sheet的excel文件(所有页共用一个数据)
+//     * @param templatePath 模板文件路径
+//     * @param outputFile 输出文件路径
+//     * @param dataMap 数据map
+//     * @param sheetCount 模板sheet数量
+//     * @return 根据模板生成的文件
+//     * @throws IOException io异常
+//     */
+//    public static File exportWithMultiSheet(String templatePath, File outputFile, Map<String, Object> dataMap, int sheetCount) throws IOException {
+//        TemplateExportParams params = new TemplateExportParams(templatePath);
+//
+//        // 开启横向遍历 开启横向遍历 开启横向遍历
+//        params.setColForEach(true);
+//        // 设置sheet页对应角标
+//        params.setSheetNum(IntStream.range(0, sheetCount).boxed().toArray(Integer[]::new));
+//        // 构造数据
+//        Map<Integer, Map<String, Object>> realMap = new HashMap<>();
+//        for (int i = 0; i < sheetCount; i++) {
+//            realMap.put(i, dataMap);
+//        }
+//        return exportExcel(realMap, params, outputFile);
+//    }
+//
+//    public static int getSheetCount(File excelFile) throws IOException {
+//        Workbook wb = WorkbookFactory.create(excelFile);
+//        try {
+//            return wb.getNumberOfSheets();
+//        } finally {
+//            IOUtils.close(wb);
+//        }
+//    }
+//
+//    public static File exportExcel(Map<Integer, Map<String, Object>> sheetDataMap, TemplateExportParams params, File outputFile) throws IOException {
+//        Workbook workbook = ExcelExportUtil.exportExcel(sheetDataMap, params);
 //        FileOutputStream out = null;
 //        try {
 //            out = new FileOutputStream(outputFile);
@@ -62,4 +113,6 @@ public class ExcelUtils {
 //            IOUtils.close(workbook, out);
 //        }
 //    }
+
+
 }
