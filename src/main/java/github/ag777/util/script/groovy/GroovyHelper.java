b13f55139ed8dce7f01a5b686a2bea7d4c513be5
ag777 <aaaggg777@qq.com>
2024-05-16 15:09:13 +0800

    补充 groovy相关工具类实现

--- src/main/java/github/ag777/util/script/groovy/GroovyHelper.java
+++ src/main/java/github/ag777/util/script/groovy/GroovyHelper.java
@@ -1,10 +1,13 @@
 package github.ag777.util.script.groovy;
 
+import groovy.lang.GroovyClassLoader;
 import groovy.util.GroovyScriptEngine;
 import groovy.util.ResourceException;
 import groovy.util.ScriptException;
+import org.codehaus.groovy.control.CompilerConfiguration;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -18,15 +21,90 @@ public class GroovyHelper {
         this.engine = engine;
     }
 
+    /**
+     * 创建一个新的GroovyHelper实例。
+     * @param baseDir Groovy脚本的基础目录路径。
+     * @return 返回一个初始化好的GroovyHelper实例。
+     * @throws IOException 如果在初始化过程中读取目录失败。
+     */
     public static GroovyHelper newInstance(String baseDir) throws IOException {
         GroovyScriptEngine engine = new GroovyScriptEngine(baseDir);
         return new GroovyHelper(engine);
     }
 
+    /**
+     * 创建一个新的GroovyHelper实例，并通过指定的包名添加导入。
+     *
+     * @param baseDir Groovy脚本的基础目录路径。
+     * @param packageNames 要添加星号导入的包名数组。
+     * @return 返回一个配置了指定导入的GroovyHelper新实例。
+     * @throws IOException 如果在实例创建过程中遇到IO异常。
+     */
+    public static GroovyHelper newInstanceWithImports(String baseDir, String... packageNames) throws IOException {
+        // 配置编译器以添加星号导入
+        CompilerConfiguration config = new CompilerConfiguration();
+        config.addCompilationCustomizers(new org.codehaus.groovy.control.customizers.ImportCustomizer().addStarImports(packageNames));
+        return newInstance(baseDir, config); // 创建并返回配置了导入的GroovyHelper实例
+    }
+
+    /**
+     * 创建一个新的GroovyHelper实例，允许通过CompilerConfiguration自定义编译器配置。
+     * @param baseDir Groovy脚本的基础目录路径。
+     * @param config 编译器配置，允许自定义Groovy编译选项。
+     * @return 返回一个初始化好的GroovyHelper实例。
+     * @throws IOException 如果在初始化过程中读取目录失败。
+     */
+    public static GroovyHelper newInstance(String baseDir, CompilerConfiguration config) throws IOException {
+        GroovyClassLoader classLoader = new GroovyClassLoader(Thread.currentThread().getContextClassLoader(), config);
+        return newInstance(baseDir, classLoader);
+    }
+
+    /**
+     * 创建一个新的GroovyHelper实例，允许通过自定义的GroovyClassLoader来加载类。
+     * @param baseDir Groovy脚本的基础目录路径。
+     * @param classLoader 自定义的Groovy类加载器。
+     * @return 返回一个初始化好的GroovyHelper实例。
+     * @throws IOException 如果在初始化过程中读取目录失败。
+     */
+    public static GroovyHelper newInstance(String baseDir, GroovyClassLoader classLoader) throws IOException {
+        GroovyScriptEngine engine = new GroovyScriptEngine(baseDir, classLoader);
+        return new GroovyHelper(engine);
+    }
+
+    /**
+     * 执行指定脚本中的方法。
+     * @param scriptName 脚本名称，指定要执行的Groovy脚本文件的名称。
+     * @param methodName 要调用的方法名。
+     * @param params 方法参数，可变参数，传入方法调用所需的参数。
+     * @return 执行结果，返回类型为泛型T，由具体执行的方法决定。
+     * @throws ScriptException 如果脚本执行出错。
+     * @throws ResourceException 如果资源访问出错。
+     * @throws InstantiationException 如果实例化异常发生。
+     * @throws IllegalAccessException 如果访问权限不足。
+     * @throws ClassNotFoundException 如果指定的类找不到。
+     */
     public <T>T exec(String scriptName, String methodName, Object... params) throws ScriptException, ResourceException, InstantiationException, IllegalAccessException {
         return GroovyUtils.exec(engine, scriptName, methodName, params);
     }
 
+    /**
+     * 获取脚本中所有方法及其参数列表。
+     * @param scriptName 脚本名称，指定要查询方法的Groovy脚本文件的名称。
+     * @return 方法信息列表，返回一个包含方法名和参数列表的MethodInfo对象的列表。
+     * @throws ClassNotFoundException 如果指定的类找不到。
+     */
+    public List<GroovyUtils.MethodInfo> getMethodsAndParameters(String scriptName) throws ClassNotFoundException {
+        return GroovyUtils.getMethodsAndParameters(engine, scriptName);
+    }
+
+    /**
+     * 使用Map形式的参数执行指定脚本中的方法。
+     * @param scriptName 脚本名称，指定要执行的Groovy脚本文件的名称。
+     * @param params 方法参数，以Map形式传入，键为参数名，值为参数值。
+     * @return 执行结果，返回类型为泛型T，由具体执行的方法决定。
+     * @throws ScriptException 如果脚本执行出错。
+     * @throws ResourceException 如果资源访问出错。
+     */
     public <T>T exec(String scriptName, Map<String, Object> params) throws ResourceException, ScriptException {
         return GroovyUtils.exec(engine, scriptName, params);
     }
--- src/main/java/github/ag777/util/script/groovy/GroovyUtils.java
+++ src/main/java/github/ag777/util/script/groovy/GroovyUtils.java
@@ -5,7 +5,13 @@ import groovy.lang.*;
 import groovy.util.GroovyScriptEngine;
 import groovy.util.ResourceException;
 import groovy.util.ScriptException;
+import lombok.Data;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -35,7 +41,70 @@ public class GroovyUtils {
     }
 
     /**
-     * 
+     * 获取指定 Groovy 脚本中所有方法的信息。
+     * <p>
+     * 该方法使用 GroovyScriptEngine 加载指定的 Groovy 脚本，并提取其中定义的所有方法。
+     * 对于每个方法，它将收集方法的名称以及其参数的详细信息（包括类型和名称）。
+     * <p>
+     * 注意：此方法不处理 Groovy 生成的合成方法。
+     *
+     * @param engine GroovyScriptEngine 实例，用于加载和解析 Groovy 脚本。
+     * @param scriptName 要加载的 Groovy 脚本的名称。
+     * @return 包含方法信息的 MethodInfo 对象列表。
+     * @throws ClassNotFoundException 如果无法找到或加载指定的脚本类。
+     */
+    public static List<MethodInfo> getMethodsAndParameters(GroovyScriptEngine engine, String scriptName) throws ClassNotFoundException {
+        List<MethodInfo> methodsList = new ArrayList<>();
+
+        // 使用 GroovyClassLoader 从引擎中加载 Groovy 脚本类
+        Class<?> scriptClass = engine.getGroovyClassLoader().loadClass(scriptName);
+
+        // 获取 Groovy 脚本中声明的所有方法
+        Method[] methods = scriptClass.getDeclaredMethods();
+
+        // 遍历所有方法
+        for (Method method : methods) {
+            // 跳过 Groovy 生成的合成方法和 main 方法
+            if (!method.isSynthetic() && !isMainMethod(method)) {
+                MethodInfo methodInfo = new MethodInfo();
+                List<ParameterInfo> paramsList = new ArrayList<>();
+
+                // 获取每个方法的参数
+                for (Parameter parameter : method.getParameters()) {
+                    ParameterInfo paramInfo = new ParameterInfo();
+                    // 将参数类型和名称设置到参数信息对象中
+                    paramInfo.setType(parameter.getType().getSimpleName());
+                    paramInfo.setName(parameter.getName());
+                    // 将参数信息对象添加到参数列表中
+                    paramsList.add(paramInfo);
+                }
+
+                // 设置方法名和参数列表到方法信息对象中
+                methodInfo.setName(method.getName());
+                methodInfo.setParams(paramsList);
+
+                // 将方法信息对象添加到方法列表中
+                methodsList.add(methodInfo);
+            }
+        }
+
+        return methodsList;
+    }
+
+    /**
+     * 检查方法是否为 main 方法
+     */
+    private static boolean isMainMethod(Method method) {
+        int modifiers = method.getModifiers();
+        Class<?>[] parameterTypes = method.getParameterTypes();
+        return Modifier.isPublic(modifiers) && Modifier.isStatic(modifiers) &&
+                "main".equals(method.getName()) &&
+                parameterTypes.length == 1 &&
+                parameterTypes[0] == String[].class;
+    }
+
+    /**
+     *
      * @param groovyShell 引擎
      * @param content 脚本字符串
      * @return 脚本
@@ -120,5 +189,17 @@ public class GroovyUtils {
         Script script = getScript(gs, text);
         exec(script, MapUtils.of("a", 1, "b", 2));
         exec(script, MapUtils.of("a", 3));
+    }
+
+    @Data
+    public static class MethodInfo {
+        private String name;
+        private List<ParameterInfo> params;
+    }
+
+    @Data
+    public static class ParameterInfo {
+        private String type;
+        private String name;
     }
 }
