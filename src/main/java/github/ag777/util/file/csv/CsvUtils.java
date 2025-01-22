d6af214a919916175579b86826e84adefa88f9e2
ag777 <aaaggg777@qq.com>
2025-01-22 20:57:28 +0800

    增加 CSV 文件写入功能

--- src/main/java/github/ag777/util/file/csv/CsvUtils.java
+++ src/main/java/github/ag777/util/file/csv/CsvUtils.java
@@ -3,12 +3,11 @@ package github.ag777.util.file.csv;
 import com.ag777.util.lang.Console;
 import com.ag777.util.lang.IOUtils;
 import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -16,9 +15,9 @@ import java.util.Map;
 import java.util.function.Consumer;
 
 /**
+ * 对commons-csv的二次封装
  * @author ag777 <837915770@vip.qq.com>
- * @Description 对commons-csv的二次封装
- * @Date 2022/7/21 14:58
+ * @version  2025/1/22 20:56
  */
 public class CsvUtils {
 
@@ -63,6 +62,60 @@ public class CsvUtils {
             IOUtils.close(reader);
         }
     }
+
+    /**
+     * 将数据列表转换为CSV格式字符串
+     *
+     * @param dataList 数据列表，每个元素是一个Object数组，包含清单名和标签列表
+     * @param headers 标题
+     * @return CSV格式的内容
+     * @throws IOException IO异常
+     */
+    public static String toStr(List<Object[]> dataList, String[] headers) throws IOException {
+        StringWriter writer = new StringWriter();
+        write(dataList, headers, writer);
+        return writer.toString();
+    }
+
+    /**
+     * 将数据列表写入CSV文件
+     *
+     * @param dataList 数据列表，每个元素是一个Object数组，包含清单名和标签列表
+     * @param headers 标题
+     * @param filePath 输出文件路径
+     * @throws IOException IO异常
+     */
+    public static void write(List<Object[]> dataList, String[] headers, String filePath) throws IOException {
+        try (Writer writer = new FileWriter(filePath, StandardCharsets.UTF_8)) {
+            write(dataList, headers, writer);
+        }
+    }
+
+    /**
+     * 将数据写入Writer
+     *
+     * @param dataList 数据列表
+     * @param headers 标题
+     * @param writer 输出writer
+     * @throws IOException IO异常
+     */
+    public static void write(List<Object[]> dataList, String[] headers, Writer writer) throws IOException {
+        CSVFormat.Builder builder = CSVFormat.DEFAULT.builder();
+        if (headers != null) {
+            builder.setHeader(headers);
+        }
+        CSVFormat csvFormat = builder.build();
+
+        try (CSVPrinter csvPrinter = new CSVPrinter(writer, csvFormat)) {
+            for (Object[] data : dataList) {
+                if (data.length < 2) {
+                    continue;
+                }
+                csvPrinter.printRecord(data);
+            }
+        }
+    }
+
 
     public static void main(String[] args) throws IOException {
         String filePath = "D:\\temp\\程序测试\\111.csv";
