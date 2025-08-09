65355eeac8ffa1ab857ae7fbfff29b10c4233b8e
ag777 <aaaggg777@qq.com>
2025-08-09 21:38:23 +0800

    漏提交

---
+++ src/main/java/github/ag777/util/remote/ai/spring/model/ChatEvents.java
@@ -0,0 +1,11 @@
+package github.ag777.util.remote.ai.spring.model;
+
+public enum ChatEvents {
+    THINKING_START,
+    THINKING,
+    THINKING_END,
+    MESSAGE_START,
+    MESSAGE,
+    MESSAGE_END,
+    COMPLETE;
+}
